247ecf946893f2da8b20404bf97be8195770ddf3
whiteleaf03 <2876202234@qq.com>
2022-07-01 23:53:09 +0800

    v0.6

---
+++ src/main/java/cn/whiteleaf03/blog/entity/ToEmail.java
@@ -0,0 +1,30 @@
+package cn.whiteleaf03.blog.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToEmail implements Serializable {
+
+    /**
+     * 邮件接收方，可多人
+     */
+    private String[] tos;
+    /**
+     * 邮件主题
+     */
+    private String subject;
+    /**
+     * 邮件内容
+     */
+    private String content;
+
+    @Value("${spring.mail.username}")
+    private String from;
+}
